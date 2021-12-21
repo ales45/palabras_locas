db36ed85818fb088ca8ecf1d203382d62b85b88c
ales <algamer451@gmail.com>
2021-12-20 23:29:09 -0500

    palabras locas v0.1

---
+++ codigo.py
@@ -0,0 +1,11 @@
+def variables():
+    sujetoFemenino1 = input("sujetoFemenino--> ")
+    cosaTransparente = input("cosaTransparente--> ")
+    sujetoMasculino2 = input("sujetoMasculino--> ")
+    verbo = input("verbo--> ")
+    return sujetoFemenino1,cosaTransparente,sujetoMasculino2,verbo
+
+def hola():
+    print("hola")
+
+hola()
---
+++ palabraslocas.py
@@ -0,0 +1,12 @@
+from codigo import variables
+# concatenar cadenas de caradteres
+
+# palabra = input("-->")
+
+# print(f"aprende a programar con {palabra}")
+
+sujetoFemenino1,cosaTransparente,sujetoMasculino2,verbo = variables()
+
+
+historia = f"en un mundo muy muy lejado una {sujetoFemenino1}, estaba mirando por una gran {cosaTransparente}, y se dio cuenta de que un {sujetoMasculino2} estaba {verbo}, la {sujetoFemenino1} se le quedo viendo como el {sujetoMasculino2} {verbo} "
+print(historia)
